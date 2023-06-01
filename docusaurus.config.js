091df7b1c7825ae08e1af7ae1c15f17e7ffc4b5a
0div <tgft73@gmail.com>
2023-06-01 16:04:04 +0200

    remove basepath

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   url: "https://docs.friendlycaptcha.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: `/${projectName}/`,
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
