43dd153aa2bdfbdf609b6e2ec260415b501ed2de
0div <tgft73@gmail.com>
2023-06-08 15:01:20 +0200

    replace production url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: "img/favicon.svg",
 
   // Set the production url of your site here
-  url: "https://docs.friendlycaptcha.com",
+  url: "https://developer.friendlycaptcha.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
