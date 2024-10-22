11b560df0bfa2985a7e6699e8f608d91daf8a4a1
merlinfuchs <merlinfuchs2001@gmail.com>
2024-10-22 12:46:51 +0200

    index pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,6 +44,7 @@ const config = {
       /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
       {
         hashed: true,
+        indexPages: true,
       },
     ],
   ],
