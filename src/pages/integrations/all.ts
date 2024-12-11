ea3c331614581e396907ece8205445e2c95a8374
Aaron Greenberg <aaron@friendlycaptcha.com>
2024-12-11 13:30:01 -0500

    Add images

--- src/pages/integrations/all.ts
+++ src/pages/integrations/all.ts
@@ -5,6 +5,7 @@ export interface Integration {
   slug: string;
   tags: Tag[];
   fcVersion: "v1" | "v2";
+  image: string;
 }
 
 export const INTEGRATIONS: Integration[] = [
@@ -13,66 +14,77 @@ export const INTEGRATIONS: Integration[] = [
     slug: "wordpress",
     tags: ["CMS"],
     fcVersion: "v2",
+    image: "wordpress.png",
   },
   {
     name: "WordPress",
     slug: "v1/wordpress",
     tags: ["CMS"],
     fcVersion: "v1",
+    image: "wordpress.png",
   },
   {
     name: "Magento 2",
     slug: "magento-2",
     tags: ["E-commerce"],
     fcVersion: "v1",
+    image: "magento-2.svg",
   },
   {
     name: "PHP",
     slug: "php",
     tags: ["Library"],
     fcVersion: "v2",
+    image: "php.svg",
   },
   {
     name: "Python",
     slug: "python",
     tags: ["Library"],
     fcVersion: "v2",
+    image: "python.svg",
   },
   {
     name: "Go",
     slug: "golang",
     tags: ["Library"],
     fcVersion: "v2",
+    image: "golang.svg",
   },
   {
     name: "Go",
     slug: "v1/golang",
     tags: ["Library"],
     fcVersion: "v1",
+    image: "golang.svg",
   },
   {
     name: "JavaScript",
     slug: "js",
     tags: ["Library"],
     fcVersion: "v2",
+    image: "js.svg",
   },
   {
     name: "JVM",
     slug: "jvm",
     tags: ["Library"],
     fcVersion: "v1",
+    image: "java.svg",
   },
   {
     name: "Android",
     slug: "android",
     tags: ["Mobile"],
     fcVersion: "v2",
+    image: "android.svg",
   },
   {
     name: "iOS",
     slug: "ios",
     tags: ["Mobile"],
     fcVersion: "v2",
+    image: "ios.svg",
   },
 ];
 
--- src/pages/integrations/index.tsx
+++ src/pages/integrations/index.tsx
@@ -15,36 +15,72 @@ export default function Integrations() {
       : selectedTags.concat("v2", searchQuery);
 
     const stringified = JSON.stringify(integration).toLowerCase();
-    return tags.every(tag => stringified.includes(tag.toLowerCase()))
+    return tags.every((tag) => stringified.includes(tag.toLowerCase()));
   });
+
+  const phpVisible = !!integrations.find((i) => i.slug === "php");
+  const androidVisible = !!integrations.find((i) => i.slug === "android");
 
   return (
     <Layout title="Integrations">
-      <div className="hero">
+      <div className="hero padding-bottom--none">
         <div className="container">
           <h1 className="hero__title">Integrations</h1>
           <p className="hero__subtitle">
             Plugins and libraries to implement Friendly Captcha into your
             product easily.
           </p>
-          <FilterBar
-            selectedTags={selectedTags}
-            setSelectedTags={setSelectedTags}
-            searchQuery={searchQuery}
-            setSearchQuery={setSearchQuery}
-          />
-          <div className={clsx(styles.grid, "margin-top--md")}>
-            {integrations.map((integration) => (
-              <IntegrationCard key={integration.slug} {...integration} />
-            ))}
-          </div>
         </div>
       </div>
+      <FilterBar
+        selectedTags={selectedTags}
+        setSelectedTags={setSelectedTags}
+        searchQuery={searchQuery}
+        setSearchQuery={setSearchQuery}
+      />
+      <div className={clsx(styles.grid, "container margin-vert--md")}>
+        {integrations.map((integration) => (
+          <IntegrationCard key={integration.slug} {...integration} />
+        ))}
+      </div>
+      <LicensingFooter android={androidVisible} php={phpVisible} />
     </Layout>
   );
 }
 
-const IntegrationCard = ({ name, fcVersion }: Integration) => (
+const LicensingFooter = ({
+  android,
+  php,
+}: {
+  android?: boolean;
+  php?: boolean;
+}) => {
+  if (!php && !android) {
+    return null;
+  }
+  return (
+    <div className="container margin-bottom--md">
+      {php ? (
+        <p className="text--italic margin--none"><small>
+          The PHP logo created by Colin Viebrock is licensed under{" "}
+          <a href="https://creativecommons.org/licenses/by-sa/4.0/">
+            CC BY-SA 4.0
+          </a>
+          .
+          </small></p>
+      ) : null}
+      {android ? (
+        <p className="text--italic margin--none"><small>
+          The Android robot is reproduced or modified from work created and
+          shared by Google and used according to terms described in the Creative
+          Commons 3.0 Attribution License.
+        </small></p>
+      ) : null}
+    </div>
+  );
+};
+
+const IntegrationCard = ({ name, fcVersion, image }: Integration) => (
   <div className={clsx("card", styles.grid__item)}>
     <div className={clsx("card__header", styles.grid__item__header)}>
       <h3>{name}</h3>
@@ -56,6 +92,9 @@ const IntegrationCard = ({ name, fcVersion }: Integration) => (
       >
         {fcVersion}
       </span>
+    </div>
+    <div className={clsx("card__body", styles.grid__item__body)}>
+      <img src={`/img/integrations/${image}`} alt={`${name} logo`} />
     </div>
     <div className="card__footer">
       <button className="button button--secondary button--block">
@@ -72,83 +111,90 @@ interface FilterBarProps {
   setSearchQuery: (query: string) => void;
 }
 
-function FilterBar({ selectedTags, setSelectedTags, searchQuery, setSearchQuery }: FilterBarProps) {
+function FilterBar({
+  selectedTags,
+  setSelectedTags,
+  searchQuery,
+  setSearchQuery,
+}: FilterBarProps) {
   return (
-    <div
-      className={clsx("container padding--none padding-top--md", styles.filter)}
-    >
-      <div
-        className={clsx(
-          "navbar__search margin-right--md",
-          styles.filter__search
-        )}
-      >
-        <input
-          className="navbar__search-input"
-          placeholder="Search integrations"
-          onInput={(e) => { setSearchQuery((e.target as HTMLInputElement).value) }}
-        />
-      </div>
-      <div
-        className={clsx(
-          "dropdown dropdown--hoverable dropdown--right",
-          styles.filter__dropdown
-        )}
-      >
-        <button className="button button--primary padding-horiz--sm">
-          <IconFilter />
-          {selectedTags.length ? selectedTags[0] : "All"}
-        </button>
-        <ul className="dropdown__menu">
-          <li>
-            <a className="dropdown__link" onClick={() => setSelectedTags([])}>
-              All
-            </a>
+    <div className="container">
+      <div className={clsx("padding-top--md", styles.filter)}>
+        <div
+          className={clsx(
+            "navbar__search margin-right--md",
+            styles.filter__search
+          )}
+        >
+          <input
+            className="navbar__search-input"
+            placeholder="Search integrations"
+            onInput={(e) => {
+              setSearchQuery((e.target as HTMLInputElement).value);
+            }}
+          />
+        </div>
+        <div
+          className={clsx(
+            "dropdown dropdown--hoverable dropdown--right",
+            styles.filter__dropdown
+          )}
+        >
+          <button className="button button--primary padding-horiz--sm">
+            <IconFilter />
+            {selectedTags.length ? selectedTags[0] : "All"}
+          </button>
+          <ul className="dropdown__menu">
+            <li>
+              <a className="dropdown__link" onClick={() => setSelectedTags([])}>
+                All
+              </a>
+            </li>
+            {Array.from(TAGS).map((tag: Tag) => (
+              <li key={tag}>
+                <a
+                  className="dropdown__link"
+                  onClick={() => {
+                    setSelectedTags([tag]);
+                  }}
+                >
+                  {tag}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <ul className="pills margin--none">
+          <li
+            className={clsx(
+              "pills__item",
+              selectedTags.length || "pills__item--active"
+            )}
+            value={searchQuery}
+            onClick={() => setSelectedTags([])}
+          >
+            All
           </li>
           {Array.from(TAGS).map((tag: Tag) => (
-            <li key={tag}>
-              <a
-                className="dropdown__link"
-                onClick={() => {
-                  setSelectedTags([tag]);
-                }}
-              >
-                {tag}
-              </a>
+            <li
+              key={tag}
+              onClick={() => {
+                if (selectedTags.includes(tag)) {
+                  setSelectedTags(selectedTags.filter((t) => t !== tag));
+                } else {
+                  setSelectedTags(selectedTags.concat(tag));
+                }
+              }}
+              className={clsx(
+                "pills__item",
+                selectedTags.includes(tag) && "pills__item--active"
+              )}
+            >
+              {tag}
             </li>
           ))}
         </ul>
       </div>
-      <ul className="pills margin--none">
-        <li
-          className={clsx(
-            "pills__item",
-            selectedTags.length || "pills__item--active"
-          )}
-          value={searchQuery}
-          onClick={() => setSelectedTags([])}
-        >
-          All
-        </li>
-        {Array.from(TAGS).map((tag: Tag) => (
-          <li
-            key={tag}
-            onClick={() => {
-              if (selectedTags.includes(tag)) {
-                setSelectedTags(selectedTags.filter((t) => t !== tag));
-              } else {
-                setSelectedTags(selectedTags.concat(tag));
-              }
-            }}
-            className={clsx(
-              "pills__item",
-              selectedTags.includes(tag) && "pills__item--active"
-            )}
-          >
-            {tag}
-          </li>
-        ))}
-      </ul>
     </div>
   );
 }
