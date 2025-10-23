456f3937274dfb60a0d51a5924a8d8b2795ae516
merlinfuchs <merlinfuchs2001@gmail.com>
2025-10-23 15:17:13 +0200

    fix tailwind build errors

--- src/plugins/tailwind-config.js
+++ src/plugins/tailwind-config.js
@@ -2,7 +2,10 @@ module.exports = function tailwindPlugin(context, options) {
   return {
     name: "tailwind-plugin",
     configurePostCss(postcssOptions) {
-      postcssOptions.plugins.push(require("@tailwindcss/postcss"));
+      postcssOptions.plugins.push(
+        require("postcss-import"),
+        require("@tailwindcss/postcss")
+      );
       return postcssOptions;
     },
   };
